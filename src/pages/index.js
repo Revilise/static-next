d25a3deae945144b3ef04afcb26bf09973259198
Revilise <nice.mutnykh@mail.ru>
2023-03-22 21:36:11 +0300

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import Layout from "../components/layout";
 import {useEffect, useState} from "react";
 
-async function fetchHtml() {
-    const text = await import("../../public/html/_index.html.txt");
-    return text.default;
-}
+
 
 export default function IndexPage() {
     const [content, SetContent] = useState("");
-    useEffect(async () => {
-        SetContent(await fetchHtml());
+    useEffect(() => {
+        async function fetchHtml() {
+            const text = await import("../../public/html/_index.html.txt");
+            return SetContent(text.default);
+        }
+        fetchHtml();
     }, [])
     return (
         <Layout title={"test"}>
